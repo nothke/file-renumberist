5bc6cec6fd3c41cc3055b6409b0301d5c438e2c0
Ivan Notaros <nothke@gmail.com>
2022-07-07 11:32:57 +0200

    Recursive

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,48 @@
 use std::env;
 use std::ffi::OsStr;
-use std::fs;
+use std::fs::{self};
+use std::io::Result;
+use std::path::PathBuf;
 
-//fn wait() {}
+fn rename_files_in_folder(dir: &PathBuf) -> Result<()> {
+    let files = fs::read_dir(dir.clone())?;
+
+    let mut i = 0;
+    for dir_entry in files {
+        let dir_entry_uw = dir_entry.unwrap();
+        println!("-- found file: {}", dir_entry_uw.path().display());
+        let path = dir_entry_uw.path();
+
+        if path.is_dir() {
+            rename_files_in_folder(&path)?; // recurse
+            continue;
+        }
+
+        // Make sure the extension doesn't change
+        let extension = match path.extension() {
+            Some(str) => str,
+            None => OsStr::new(""),
+        };
+
+        //println!("- extension: {}", extension.to_str().unwrap());
+
+        let mut new_path = dir.clone();
+        new_path.push(format!("{}.{}", i, extension.to_str().unwrap()));
+
+        println!(
+            "-- Renaming: {} to {}.{}",
+            new_path.as_path().display(),
+            i,
+            extension.to_str().unwrap()
+        );
+
+        fs::rename(dir_entry_uw.path(), new_path.as_path())?;
+
+        i += 1;
+    }
+
+    Ok(())
+}
 
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
@@ -17,38 +57,7 @@ fn main() -> std::io::Result<()> {
 
     println!("READING DIR: {}", target_dir.display());
 
-    let files = fs::read_dir(target_dir.clone())?;
-
-    let mut i = 0;
-    for dir_entry in files {
-        let dir_entry_uw = dir_entry.unwrap();
-        println!("-- found file: {}", dir_entry_uw.path().display());
-        let path = dir_entry_uw.path();
-
-        if path.is_dir() {
-            continue;
-        }
-
-        // Make sure the extension doesn't change
-        let extension = match path.extension() {
-            Some(str) => str,
-            None => OsStr::new(""),
-        };
-
-        //println!("- extension: {}", extension.to_str().unwrap());
-
-        let mut new_path = target_dir.clone();
-        new_path.push(format!("{}.{}", i, extension.to_str().unwrap()));
-
-        println!(
-            "-- attempting to rename to: {}",
-            new_path.as_path().display()
-        );
-
-        fs::rename(dir_entry_uw.path(), new_path.as_path())?;
-
-        i += 1;
-    }
+    rename_files_in_folder(&target_dir)?;
 
     Ok(())
 }
